3110619f1ecbe12db8f2b1843f9eaedaa38ac407
Layne Penney <layne1penney@gmail.com>
2016-10-21 00:19:02 -0500

    python script starter

---
+++ cookie/README.md
@@ -0,0 +1,21 @@
+# dsci
+
+conda things
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ dsci --help
+
---
+++ cookie/dsci/cli.py
@@ -0,0 +1,37 @@
+import click
+import os
+
+
+@click.group(name='dsci')
+@click.option('--home', default=None)
+@click.pass_context
+def main(ctx, home):
+    """dsci env tool"""
+    home = os.path.abspath(home or '.')
+    ctx.obj = home
+    # click.echo('home={0}'.format(home))
+
+
+@main.command()
+@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+@click.argument('name', default='world', required=False)
+def hello(name, as_cowboy):
+    """say hello"""
+    greet = 'Howdy' if as_cowboy else 'Hello'
+    click.echo('{0}, {1}.'.format(greet, name))
+
+
+@main.command()
+@click.option('--env', '-e', default='dsienv')
+@click.pass_obj
+def activate(home, env):
+    """activate dsci env"""
+    click.echo("home={0}, activate env={1}".format(home, env))
+
+
+@main.command()
+@click.argument('--env', '-e', default='dsienv', required=False)
+@click.pass_obj
+def deactivate(home, env):
+    """deactivate dsci env"""
+    click.echo("home={0}, deactivate env={1}".format(home, env))
